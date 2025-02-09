c33e676e3bcde54508c9acf7c0b2ecd9c19259e3
happybyr <1021629211@qq.com>
2025-02-09 20:06:13 +0800

    simplestorage and storagefactory

---
+++ contracts/storagefactory/StorageFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(simpleStorage);
+    }
+
+    function sfStorage(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+}
